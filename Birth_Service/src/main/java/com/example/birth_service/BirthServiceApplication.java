70cd05ea1e4a8f77586ec72f62efc84b026624d0
leeeunhak-123 <121605778+leeeunhak-123@users.noreply.github.com>
2024-01-14 20:17:51 +0900

    second_upload
    wa ha_2

---
+++ Birth_Service/src/main/java/com/example/birth_service/BirthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.birth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthServiceApplication.class, args);
+	}
+
+}
